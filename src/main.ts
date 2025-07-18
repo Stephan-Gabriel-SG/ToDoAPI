8ff5ed8f5609d43d05d87ef7b0e38c498e4cd6f1
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-07-18 18:20:18 +0300

    add validation options

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        // excludeExtraneousValues: true,
+        exposeDefaultValues: true,
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Error during bootstrapping:', err);
+  process.exit(1);
+});
