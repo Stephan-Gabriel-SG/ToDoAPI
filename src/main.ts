1d0399961030458bb2bd7c5dd52109a0556e5ed4
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-07-20 05:53:36 +0300

    import ServeStaticFile

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
     new ValidationPipe({
       transform: true,
       transformOptions: {
-        // excludeExtraneousValues: true,
         exposeDefaultValues: true,
       },
     }),
